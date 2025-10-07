5fed829c048dfd3d3332a494e0336346a43e787d
imanka <imankasilvatharusha@gmail.com>
2025-10-07 14:40:48 +0530

    loging

--- src/App.jsx
+++ src/App.jsx
@@ -2,24 +2,14 @@ import { useState } from 'react'
 import './App.css'
 import ProductCard  from './components/product-card' 
 import Header from './components/header'
+import LoginPage from './pages/loginPage'
 
 
 function App() {
   
 
   return (
-  <div className='w-full h-screen bg-pink-200'>
-    <div className='w-[500px] flex flex-col items-center h-[500px] bg-gray-200 relative'>
-      <div className='w-[90px] h-[90px] bg-red-500'>
-      </div>
-      <div className='w-[90px] h-[90px] bg-yellow-500 absolute right-[50px] top-[50px]'>
-      </div>
-      <div className='w-[90px] h-[90px] bg-green-500'>
-      </div>
-      <div className='w-[90px] h-[90px] bg-blue-500 fixed right-[50px] bottom-[50px]'>
-      </div>
-    </div>
-  </div>
+      <LoginPage/>
   )
 }
 
---
+++ src/pages/loginPage.jsx
@@ -0,0 +1,7 @@
+export default function LoginPage() {
+  return (
+    <div className="w-full bg-red-900 h-screen">
+        <h1>Loggin Page</h1>
+    </div>
+  )
+}
