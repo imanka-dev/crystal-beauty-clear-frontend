5b54c338e49ea98631deb9068e7ede59606ccf79
imanka <imankasilvatharusha@gmail.com>
2025-10-11 20:27:25 +0530

    ab

--- src/pages/loginPage.jsx
+++ src/pages/loginPage.jsx
@@ -1,12 +1,14 @@
 import axios from "axios";
 import { useState } from "react";
 import toast from "react-hot-toast";
+import { useNavigate } from "react-router-dom";
 
 
 export default function LoginPage() {
 
   const [email,setEmail] = useState("")
   const [password,setPassword] = useState("")
+  const  navigate =useNavigate()
 
 
   function handleLogin(){
@@ -30,9 +32,9 @@ export default function LoginPage() {
      
     const user = response.data.user;
     if(user.role==="admin"){
-        window.location.href="/admin"
+        navigate("/admin")
     }else{
-        window.location.href="/"
+        navigate("/")
     }
   }
 })
