39350064f8e1f72499ca5dd786890c6dcebd16a7
imanka <imankasilvatharusha@gmail.com>
2025-10-13 23:27:05 +0530

    v13add table

--- src/pages/admin/products.jsx
+++ src/pages/admin/products.jsx
@@ -20,8 +20,39 @@ export default function AdminProductsPage(){
 
         return(
             <div className="w-full h-full rounded-lg">
+
+                <table className="w-full">
+                    <thead>
+                        <tr>
+                           
+                            <th>Product ID</th>
+                            <th>Name</th>
+                            <th>Price</th>
+                            <th>Labeled Price</th>
+                            <th>Stock</th>
+                            
+                        </tr>
+                    </thead>
+                    <tbody>
+
+                    </tbody>
+
+                </table>
+
                 {
-                   <h1>Products</h1>
+                   products.map(
+                    (product,index)=>{
+                        console.log("mapping"+ product.productId)
+                        
+                        return(
+                            <div key={index} className="w-full h-[100px] bg-gray-300 flex items-center justify-between p-2 m-2 rounded-lg shadow-lg">
+                                
+
+                            </div>
+                            
+                        )
+                    }
+                   )
                 }
 
 
