1c22709d008effed4acd22742d420eef97dc7dac
imanka <imankasilvatharusha@gmail.com>
2025-10-15 16:33:26 +0530

    day13 add product form

--- src/pages/admin/addProduct.jsx
+++ src/pages/admin/addProduct.jsx
@@ -10,6 +10,8 @@ productId ,
 
 */
 
+import { Link } from "react-router-dom";
+
 export default function AddProductForm(){
     return(
         <div className="w-full h-full rounded-lg  flex justify-center items-center">
@@ -54,6 +56,11 @@ export default function AddProductForm(){
                  className="w-[400px] h-[50px] border border-gray-500 rounded-xl text-center m-[5px]" 
                  placeholder="Stock" 
                  />
+                 <div className="w-[400px] h-[100px] flex justify-between items-center rounded-lg">
+                
+                    <Link to={"/admin/products"} className="bg-red-500 text-white p-[10px] w-[180px] text-center rounded-lg  hover:bg-red-600 transition duration-300">Cancel</Link>
+                    <button className="bg-blue-500 text-white p-[10px] w-[180px] text-center rounded-lg cursor-pointer hover:bg-blue-600 transition duration-300">Add Product</button>
+                 </div>
           </div>
         </div>
     )
