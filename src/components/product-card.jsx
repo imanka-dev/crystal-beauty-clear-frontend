a791ec775f6c9e9cb32c288a9b8e2743996c9660
imanka <imankasilvatharusha@gmail.com>
2025-10-20 16:50:51 +0530

    v16 product overview✏

--- src/components/product-card.jsx
+++ src/components/product-card.jsx
@@ -6,10 +6,10 @@ export default function ProductCard(props){
 
         <Link className="w-[250px] m-4 h-[350px]  border shadow-2xl  ">
                 <img className="w-full h-[220px] object-cover"src={product.images[0]} />
-                <div className="h-[110px] w-full flex flex-col justify-center items-center">
-                    <p>{product.productId}</p>
-                    <p className="">{product.name}</p>
-                    <p>{product.price.toFixed(2)} <span className="line-through">{product.price<product.labeledPrice&&product.labeledPrice.toFixed(2)}</span></p>
+                <div className="h-[110px] w-full flex flex-col justify-center px-4">
+                    <p className="text-gray-400">{product.productId}</p>
+                    <p className="text-lg font-bold">{product.name}</p>
+                    <p className="text-lg text-pink-400">Rs{product.price.toFixed(2)} <span className="line-through text-gray-400 text-sm">{product.price<product.labeledPrice&&product.labeledPrice.toFixed(2)}</span></p>
                 </div>
         </Link>
 
